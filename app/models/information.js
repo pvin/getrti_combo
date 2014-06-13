c38a83dd5cebf8294fad6a6397ea283742e108ac
pvin <praaveenranganathan@gmail.com>
2014-06-13 19:56:01 +0530

    added schema to model file

--- app/models/information.js
+++ app/models/information.js
@@ -1,6 +1,18 @@
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
+//
+//module.exports = mongoose.model('Information', {
+//    name: { type: String,index: true },
+//    designation:{ type: String,index: true}
+//	//done : Boolean
+//});
 
-module.exports = mongoose.model('Information', {
-	name : String
-	//done : Boolean
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var InfromationSchema   = new Schema({
+    name: { type: String,index: true },
+    designation:{ type: String,index: true},
+    department:{ type: String,index: true}
 });
+
+module.exports = mongoose.model('Information', InfromationSchema);
